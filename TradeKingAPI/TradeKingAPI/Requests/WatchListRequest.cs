adc30ab66100263d3cbe4303c2e5ae9c13638f37
chris <chris.lu06@gmail.com>
2017-02-14 11:35:41 -0600

    watch list request

---
+++ TradeKingAPI/TradeKingAPI/Requests/WatchListRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradeKingAPI.Models.Responses;
+using TradeKingAPI.Models.WatchList;
+
+namespace TradeKingAPI.Requests
+{
+    public class WatchListRequest
+    {
+        private OAuthRequestHandler _requestHandler;
+
+        public WatchListRequest()
+        {
+            _requestHandler = new OAuthRequestHandler();
+        }
+
+        public async Task<Watchlists> Execute()
+        {
+            var url = "market/watchlist.json";
+            var resp = await _requestHandler.ExecuteRequest<WatchListResponse>(url);
+
+            return resp.Response.Watchlists;
+        }
+    }
+}
